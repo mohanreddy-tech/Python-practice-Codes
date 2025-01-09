cbdc5a38bd44a2d996eca960e95d7fe748ef3c8b
Mohan reddy <mohanreddy1234@gmail.com>
2025-01-09 11:14:47 +0530

    Initial Commit

---
+++ Stringmethods.py
@@ -0,0 +1,10 @@
+#Different ways of declaraing the strings :
+# '''
+s1 = 'My Name is mohan'
+s2 = "My age is 23"
+s3 = """We are Learning Strings"""
+s4 = '''Python is Dynamically Typed Language'''
+print(s1)
+print(s2)
+print(s3)
+print(s4)
---
+++ UseInput.py
@@ -0,0 +1,6 @@
+#input ()will always takes an input as a string:
+num1 = int(input('Enter  num1'))
+num2 = int(input('Enter num2'))
+print(f'Addition of{num1} and {num2} is {num1+num2}')
+#print('Value of num1 is:',num1, 'Data Type of num1 is:', type(num1))
+
